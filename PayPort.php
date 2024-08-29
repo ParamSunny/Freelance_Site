6e700733eb38e52f04c8b125de7f67bd720089c0
Paramveer Singh <116633565+ParamSunny@users.noreply.github.com>
2024-08-29 17:22:07 +0530

    Add files via upload

---
+++ PayPort.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Payment</title>
+    <link href="images/Freelance.png" rel="icon">
+</head>
+</html>
+
+<?php 
+    $apiKey = "rzp_test_8qgkT37UOlZAmg";
+?>
+<form action="#section_3" method="POST">
+<script
+   src="https://checkout.razorpay.com/v1/checkout.js"
+    data-key="<?php echo $apiKey; ?>"  
+    data-amount="299900" 
+    data-currency="INR"
+    data-id="<?php echo 'OID'.rand(10,100).'END'; ?>"
+    data-buttontext="Pay with Razorpay"
+    data-name="Portfolio"
+    data-description=""
+    data-image=""
+    data-prefill.name=""
+    data-prefill.email=""
+    data-theme.color="#F37254"
+></script>
+<input type="hidden" custom="Hidden Element" name="hidden"/>
+</form>
+
+<!-- <style>
+    .razorpay-payment-button{display: none;}
+</style>  -->
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('.razorpay-payment-button').click();
+    });
+</script>
---
+++ Portfolio.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name="description" content="">
+        <meta name="author" content="TemplateMo">
+
+        <title>Portfolio Making</title>
+
+        <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
+        <!-- CSS FILES -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+        <link href="images/Freelance.png" rel="icon">
+        
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
+
+        <link href="bootstrap.min.css" rel="stylesheet">
+
+        <link href="bootstrap-icons.css" rel="stylesheet">
+
+        <link href="magnific-popup.css" rel="stylesheet">
+
+        <link href="templatemo-first-portfolio-style.css" rel="stylesheet">
+        
+<!--
+
+TemplateMo 578 First Portfolio
+
+https://templatemo.com/tm-578-first-portfolio
+
+-->
+    </head>
+<body>
+    <section class="contact section-padding" id="section_5">
+        <div class="container">
+            <h6 style="color: red;">* Note you have to do both fill the form and send it or make the payment otherwise there is no reponsed you get after.</h6>
+            <div class="row">
+                <div class="col-lg-12 col-12 mt-5 mt-lg-12">
+                    <form action="https://api.web3forms.com/submit" method="POST" class="custom-form contact-form" role="form">
+                        <div class="row">
+                            <input type="hidden" name="access_key" value="35246e68-8de9-48e2-bb31-ce319c4a8a2b">
+                            <div class="col-lg-6 col-md-6 col-12">
+                                <div class="form-floating">
+                                    <input type="text" name="name" id="name" class="form-control" placeholder="Name" required="">
+                                    
+                                    <label for="floatingInput">Name</label>
+                                </div>
+                            </div>
+    
+                            <div class="col-lg-6 col-md-6 col-12"> 
+                                <div class="form-floating">
+                                    <input type="email" name="email" id="email" pattern="[^ @]*@[^ @]*" class="form-control" placeholder="Email address" required="">
+                                    
+                                    <label for="floatingInput">Email address</label>
+                                </div>
+                            </div>
+    
+                            <div class="col-lg-3 col-md-6 col-6">
+                                <div class="form-check form-check-inline">
+                                    <input name="Category" type="checkbox" class="form-check-input" id="inlineCheckbox1" value="Graphic designer">
+    
+                                    <label class="form-check-label" for="inlineCheckbox1">
+                                        <ion-icon name="pie-chart-outline"></ion-icon>
+                                        <span class="form-check-label-text">Graphic designer</span>
+                                    </label>
+                              </div>
+                            </div>
+    
+                            <div class="col-lg-3 col-md-6 col-6">
+                                <div class="form-check form-check-inline">
+                                    <input name="Category" type="checkbox" class="form-check-input" id="inlineCheckbox2" value="Architecture">
+    
+                                    <label class="form-check-label" for="inlineCheckbox2">
+                                        <ion-icon name="business-outline"></ion-icon>
+                                        <span class="form-check-label-text">Architecture</span>
+                                    </label>
+                                </div>
+                            </div>
+    
+                            <div class="col-lg-3 col-md-6 col-6">
+                                <div class="form-check form-check-inline">
+                                    <input name="Category" type="checkbox" class="form-check-input" id="inlineCheckbox3" value="Photographer">
+    
+                                    <label class="form-check-label" for="inlineCheckbox3">
+                                        <ion-icon name="camera-outline"></ion-icon>
+                                        <span class="form-check-label-text">Photographer</span>
+                                    </label>
+                                </div>
+                            </div>
+    
+                            <div class="col-lg-3 col-md-6 col-6">
+                                <div class="form-check form-check-inline me-0">
+                                    <input name="Category" type="checkbox" class="form-check-input" id="inlineCheckbox4" value="Other">
+    
+                                    <label class="form-check-label" for="inlineCheckbox4">
+                                        <ion-icon name="color-wand-outline"></ion-icon>
+                                        <span class="form-check-label-text">Other</span>
+                                    </label>
+                                </div>
+                            </div>
+    
+                            <div class="col-lg-12 col-12">
+                                <div class="form-floating">
+                                    <input class="form-control" id="message" name="message" placeholder="Tell me about the project"></input>
+                                    
+                                    <label for="floatingTextarea">Tell me about your idea</label>
+                                </div>
+                            </div>
+    
+                            <div class="col-lg-12">
+                                <button type="submit" class="form-control">Send</button>
+                            </div>
+                        </div>
+                    </form>
+                    <form action="PayPort.php" method="POST" class="custom-form contact-form" role="form" style="margin-top: 5px;">
+                        <div class="col-lg-12">
+                            <button type="submit" class="form-control">Pay now</button>
+                        </div>
+                    </form>
+                </div>
+    
+            </div>
+        </div>
+    </div>
+    </section>
+</body>
+</html>
